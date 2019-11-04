8a804a187e934ab7f90d374559a905c37d4ea78f
Jakub Dąbek <jakub.dabek@gmail.com>
2019-11-04 19:16:01 +0100

    Clean up slider code

--- lab1/game.ts
+++ lab1/game.ts
@@ -3,32 +3,43 @@ class Vec2D {
 
     public add(other: Vec2D): Vec2D { return vec2(this.x + other.x, this.y + other.y); }
     public mul(other: Vec2D): Vec2D { return vec2(this.x * other.x, this.y * other.y); }
+
+    public toString() { return `(${this.x}, ${this.y})`}
+    
+    static readonly zero = new Vec2D(0, 0);
 }
 
 const vec2 = (x: number, y: number) => new Vec2D(x, y);
 
 class Vec3D {
     constructor(public x: number, public y: number, public z: number) {}
-
+    
     public add(other: Vec3D): Vec3D { return vec3(this.x + other.x, this.y + other.y, this.z + other.z); }
     public mul(other: Vec3D): Vec3D { return vec3(this.x * other.x, this.y * other.y, this.z * other.z); }
+    public toString() { return `(${this.x}, ${this.y}, ${this.z})`}
+    
+    static readonly zero = new Vec3D(0, 0, 0);
 }
 
 const vec3 = (x: number, y: number, z: number) => new Vec3D(x, y, z);
 
 class Rotation {
     constructor(public roll: number, public pitch: number, public yaw: number) {}
-    get x() { return this.roll; }
-    get y() { return this.pitch; }
-    get z() { return this.yaw; }
+    public get x() { return this.roll; }
+    public get y() { return this.pitch; }
+    public get z() { return this.yaw; }
+    public toString() { return `(${this.roll}, ${this.pitch}, ${this.yaw})`}
+
+    public static readonly zero = new Rotation(0, 0, 0);
 }
 
 class Camera {
     constructor(
-        public position: Vec3D,
-        public rotation: Rotation,
-        public nearDistance: number,
-        public farDistance: number,
+        public position: Vec3D = Vec3D.zero,
+        public rotation: Rotation = Rotation.zero,
+        public fov: number = 0,
+        public nearDistance: number = 0,
+        public farDistance: number = 0,
     ) {}
 
     public getRelativePoint(point: Vec3D): Vec3D {
@@ -51,6 +62,10 @@ class Camera {
         const dz = cx * tmp1 - sx * tmp2;
 
         return vec3(dx, dy, dz);
+    }
+
+    public normalizeViewVolume(relativePoint: Vec3D): Vec3D {
+        return null;
     }
 
     public projection(relativePoint: Vec3D): Vec2D {
@@ -122,18 +137,41 @@ const context = canvas.getContext("2d");
 context.translate(canvas.width / 2, canvas.height / 2);
 context.scale(1, -1);
 
-const game = new GameState(new Camera(vec3(0, 0, 0), new Rotation(0, 0, 0), 1, 30), [new Box(vec3(10, 10, 10), vec3(50, 50, 50))]);
+const game = new GameState(new Camera(), [new Box(vec3(10, 10, 10), vec3(50, 50, 50))]);
 
 const withRedraw = f => () => { f(); game.draw(context); }
 
-const cameraX = <HTMLInputElement> document.getElementById("cameraX");
-cameraX.oninput = withRedraw(() => game.camera.position.x = cameraX.valueAsNumber);
-const cameraY = <HTMLInputElement> document.getElementById("cameraY");
-cameraY.oninput = withRedraw(() => game.camera.position.y = cameraY.valueAsNumber);
-const cameraZ = <HTMLInputElement> document.getElementById("cameraZ");
-cameraZ.oninput = withRedraw(() => game.camera.position.z = cameraZ.valueAsNumber);
+const updateProperty = (obj: any, path: string) => {
+    const pathSegments = path.split('.');
+    const innerObject = pathSegments.slice(0, -1).reduce((o, prop) => o[prop], obj);
+    const lastSegment = pathSegments[pathSegments.length - 1];
+
+    return (value: any) => innerObject[lastSegment] = value;
+};
+
+
+const updateOnInput = (elementId: string, path: string) => {
+    const element = <HTMLInputElement> document.getElementById(elementId);
+    const updateProp = updateProperty(game, path);
+    const update = () => updateProp(element.valueAsNumber);
+    update();
+    element.oninput = withRedraw(update);
+};
+
 
 const mainGame = () => {
+    updateOnInput("cameraX", "camera.position.x");
+    updateOnInput("cameraY", "camera.position.y");
+    updateOnInput("cameraZ", "camera.position.z");
+    
+    updateOnInput("cameraRoll", "camera.rotation.roll");
+    updateOnInput("cameraPitch", "camera.rotation.pitch");
+    updateOnInput("cameraYaw", "camera.rotation.yaw");
+    
+    updateOnInput("fov", "camera.fov");
+    updateOnInput("nearDistance", "camera.nearDistance");
+    updateOnInput("farDistance", "camera.farDistance");
+
     game.draw(context);
 };
 
