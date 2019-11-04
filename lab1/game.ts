a8821350ce555d5949435236a36932b90df5db24
Jakub Dąbek <jakub.dabek@gmail.com>
2019-11-04 22:48:38 +0100

    Add clipping

--- lab1/game.ts
+++ lab1/game.ts
@@ -5,10 +5,11 @@ class Vec2D {
     constructor(public x: number, public y: number) {}
 
     public add(other: Vec2D): Vec2D { return vec2(this.x + other.x, this.y + other.y); }
+    public sub(other: Vec2D): Vec2D { return vec2(this.x - other.x, this.y - other.y); }
     public mul(other: Vec2D): Vec2D { return vec2(this.x * other.x, this.y * other.y); }
 
     public toString() { return `(${this.x}, ${this.y})`}
-    
+
     static readonly zero = new Vec2D(0, 0);
 }
 
@@ -16,11 +17,13 @@ const vec2 = (x: number, y: number) => new Vec2D(x, y);
 
 class Vec3D {
     constructor(public x: number, public y: number, public z: number) {}
-    
+
     public add(other: Vec3D): Vec3D { return vec3(this.x + other.x, this.y + other.y, this.z + other.z); }
+    public sub(other: Vec3D): Vec3D { return vec3(this.x - other.x, this.y - other.y, this.z - other.z); }
     public mul(other: Vec3D): Vec3D { return vec3(this.x * other.x, this.y * other.y, this.z * other.z); }
+
     public toString() { return `(${this.x}, ${this.y}, ${this.z})`}
-    
+
     static readonly zero = new Vec3D(0, 0, 0);
 }
 
@@ -67,13 +70,30 @@ class Camera {
         return vec3(dx, dy, dz);
     }
 
-    public normalizeViewVolume(relativePoint: Vec3D): Vec3D {
-        const xy = this.projection(relativePoint);
+    public normalizeViewVolumePoint(p: Vec3D): Vec3D {
+        const xy = this.projection(p);
 
         const midpoint = (this.farDistance + this.nearDistance) / 2;
         const distance = this.farDistance - this.nearDistance;
 
-        return vec3(xy.x, xy.y, (relativePoint.z - midpoint) / distance);
+        return vec3(xy.x, xy.y, (p.z - midpoint) / distance);
+    }
+
+    public normalizeViewVolume([p1, p2]: [Vec3D, Vec3D]): [Vec3D, Vec3D] | null {
+        const tooNear1 = p1.z < this.nearDistance;
+        const tooNear2 = p2.z < this.nearDistance;
+        if (tooNear1 && tooNear2)
+            return null;
+        if (tooNear1 || tooNear2) {
+            const alpha = (this.nearDistance - p1.z) / (p2.z - p1.z);
+            const intersection = vec3(p1.x + alpha * (p2.x - p1.x), p1.y + alpha * (p2.y - p1.y), this.nearDistance);
+            if (tooNear1)
+                p1 = intersection;
+            else
+                p2 = intersection;
+        }
+
+        return [this.normalizeViewVolumePoint(p1), this.normalizeViewVolumePoint(p2)];
     }
 
     public projection(relativePoint: Vec3D): Vec2D {
@@ -84,23 +104,80 @@ class Camera {
     }
 }
 
-const clipLine = ([p1, p2]: [Vec3D, Vec3D]) => {
+const POSITIVE_X = 0b100000;
+const NEGATIVE_X = 0b010000;
+const POSITIVE_Y = 0b001000;
+const NEGATIVE_Y = 0b000100;
+const POSITIVE_Z = 0b000010;
+const NEGATIVE_Z = 0b000001;
+
+const clipLine = ([p1, p2]: [Vec3D, Vec3D]): [Vec3D, Vec3D] | null => {
     const outCodes = [
-        { pred: (p: Vec3D) => p.x > 1,  code: 0b100000 },
-        { pred: (p: Vec3D) => p.x < -1, code: 0b010000 },
-        { pred: (p: Vec3D) => p.y > 1,  code: 0b001000 },
-        { pred: (p: Vec3D) => p.y < -1, code: 0b000100 },
-        { pred: (p: Vec3D) => p.z > 1,  code: 0b000010 },
-        { pred: (p: Vec3D) => p.z < -1, code: 0b000001 },
+        { pred: (p: Vec3D) => p.x > 1,  code: POSITIVE_X },
+        { pred: (p: Vec3D) => p.x < -1, code: NEGATIVE_X },
+        { pred: (p: Vec3D) => p.y > 1,  code: POSITIVE_Y },
+        { pred: (p: Vec3D) => p.y < -1, code: NEGATIVE_Y },
+        { pred: (p: Vec3D) => p.z > 1,  code: POSITIVE_Z },
+        { pred: (p: Vec3D) => p.z < -1, code: NEGATIVE_Z },
     ];
 
     const getOutCode = p => outCodes.reduce((curr, {pred, code}) => pred(p) ? curr | code : curr, 0)
 
-    const out1 = getOutCode(p1);
-    const out2 = getOutCode(p2);
+    for (let i = 1; i < 10; i++) {
+        const out1 = getOutCode(p1);
+        const out2 = getOutCode(p2);
+
+        if ((out1 | out2) == 0) {
+			return [p1, p2];
+		} else if (out1 & out2) {
+			return null;
+		} else {
+            const outcode = out1 ? out1 : out2;
+            let intersection: Vec3D;
+            if (outcode & POSITIVE_X) {
+                console.log(`POSITIVE_X ${p1} -> ${p2}`);
+                const alpha = (1 - p1.x) / (p2.x - p1.x);
+                intersection = vec3(1, p1.y + alpha * (p2.y - p1.y), p1.z + alpha * (p2.z - p1.z));
+            } else if (outcode & NEGATIVE_X) {
+                console.log(`NEGATIVE_X ${p1} -> ${p2}`);
+                const alpha = (-1 - p1.x) / (p2.x - p1.x);
+                intersection = vec3(-1, p1.y + alpha * (p2.y - p1.y), p1.z + alpha * (p2.z - p1.z));
+            } else if (outcode & POSITIVE_Y) {
+                console.log(`POSITIVE_Y ${p1} -> ${p2}`);
+                const alpha = (1 - p1.y) / (p2.y - p1.y);
+                intersection = vec3(p1.x + alpha * (p2.x - p1.x), 1, p1.z + alpha * (p2.z - p1.z));
+            } else if (outcode & NEGATIVE_Y) {
+                console.log(`NEGATIVE_Y ${p1} -> ${p2}`);
+                const alpha = (-1 - p1.y) / (p2.y - p1.y);
+                intersection = vec3(p1.x + alpha * (p2.x - p1.x), -1, p1.z + alpha * (p2.z - p1.z));
+            } else if (outcode & POSITIVE_Z) {
+                console.log(`POSITIVE_Z ${p1} -> ${p2}`);
+                const alpha = (1 - p1.z) / (p2.z - p1.z);
+                intersection = vec3(p1.x + alpha * (p2.x - p1.x), p1.y + alpha * (p2.y - p1.y), 1);
+            } else if (outcode & NEGATIVE_Z) {
+                console.log(`NEGATIVE_Z ${p1} -> ${p2}`);
+                const alpha = (-1 - p1.z) / (p2.z - p1.z);
+                intersection = vec3(p1.x + alpha * (p2.x - p1.x), p1.y + alpha * (p2.y - p1.y), -1);
+            } else {
+                console.log(`bruh moment: ${p1} -> ${p2}`);
+            }
+
+            if (outcode == out1) {
+                p1 = intersection;
+            } else {
+                p2 = intersection;
+            }
+        }
+    }
+
+    console.log(`couldn't clip ${p1} -> ${p2}`);
 };
 
-class Box {
+interface Shape {
+    getLines(): [Vec3D, Vec3D][];
+}
+
+class Box implements Shape {
     constructor(public vertex: Vec3D, public size: Vec3D) {}
 
     public getLines(): [Vec3D, Vec3D][] {
@@ -135,20 +212,37 @@ class Box {
 }
 
 class GameState {
-    constructor(public camera: Camera, public boxes: Box[]) {}
+    constructor(public camera: Camera, public shapes: Shape[]) {}
 
     public draw(context: CanvasRenderingContext2D) {
         context.clearRect(-context.canvas.width / 2, - context.canvas.height / 2, context.canvas.width, context.canvas.height);
 
         context.beginPath();
-        this.boxes.forEach(box => {
+        this.shapes.forEach(box => {
             box.getLines().forEach(([begin, end]: [Vec3D, Vec3D]) => {
                 console.log(`Drawing line: ${begin} -> ${end}`);
-                const beginProjected = this.camera.projection(this.camera.getRelativePoint(begin));
-                const endProjected = this.camera.projection(this.camera.getRelativePoint(end));
-                console.log(`Projected: ${beginProjected} -> ${endProjected}`);
-                context.moveTo(beginProjected.x * context.canvas.width / 2, beginProjected.y * context.canvas.height / 2);
-                context.lineTo(endProjected.x * context.canvas.width / 2, endProjected.y * context.canvas.height / 2);
+                const beginRel = this.camera.getRelativePoint(begin);
+                const endRel = this.camera.getRelativePoint(end);
+
+                console.log(`Relative: ${beginRel} -> ${beginRel}`);
+
+                const normalized = this.camera.normalizeViewVolume([beginRel, endRel]);
+                if (normalized == null)
+                    return;
+                const [beginNormalized, endNormalized] = normalized;
+
+                console.log(`Normalized: ${beginNormalized} -> ${beginNormalized}`);
+
+                const clipped = clipLine([beginNormalized, endNormalized]);
+
+                if (clipped == null)
+                    return;
+
+                const [beginClipped, endClipped] = clipped;
+
+                console.log(`Projected: ${beginClipped} -> ${endClipped}`);
+                context.moveTo(beginClipped.x * context.canvas.width / 2, beginClipped.y * context.canvas.height / 2);
+                context.lineTo(endClipped.x * context.canvas.width / 2, endClipped.y * context.canvas.height / 2);
             });
         });
 
@@ -163,6 +257,7 @@ context.scale(1, -1);
 // context.scale(canvas.width / 2, canvas.height / 2);
 
 const game = new GameState(new Camera(), [new Box(vec3(-10, -10, 10), vec3(50, 50, 50))]);
+// const game = new GameState(new Camera(), [{getLines: ()=>[[vec3(-10, 0, 10), vec3(10, 0, 10)]]}]);
 
 const withRedraw = f => () => { f(); game.draw(context); }
 
@@ -188,11 +283,11 @@ const mainGame = () => {
     updateOnInput("cameraX", "camera.position.x");
     updateOnInput("cameraY", "camera.position.y");
     updateOnInput("cameraZ", "camera.position.z");
-    
+
     updateOnInput("cameraRoll", "camera.rotation.roll", deg2rad);
     updateOnInput("cameraPitch", "camera.rotation.pitch", deg2rad);
     updateOnInput("cameraYaw", "camera.rotation.yaw", deg2rad);
-    
+
     updateOnInput("fov", "camera.fov");
     updateOnInput("nearDistance", "camera.nearDistance");
     updateOnInput("farDistance", "camera.farDistance");
